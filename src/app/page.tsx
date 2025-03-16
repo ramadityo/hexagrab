92b0cfb6d714d5fdba0867effbc54c14c69ec47d
ramadityo <ramzprivatexd@gmail.com>
2025-03-16 15:51:56 +0700

    finishing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import UploadBox from "@/components/UploadBox";
 export default function Home() {
 
     return (
-        <div className="w-full h-screen flex items-center justify-center">
-            <div className="w-full flex justify-center">
+        <div className="w-full h-screen flex items-center justify-center" >
+            <div className="w-full flex justify-center px-6" >
                 <UploadBox />
             </div>
         </div>
