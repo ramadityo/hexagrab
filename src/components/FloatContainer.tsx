b350dd0e912462fcf0dde06562714bc8d18b3bf1
M.Fathin Halim <halimfathin7@gmail.com>
2025-04-23 18:20:08 +0700

    Hehehe

--- src/components/FloatContainer.tsx
+++ src/components/FloatContainer.tsx
@@ -4,12 +4,17 @@ import React, { useEffect, useState } from 'react'
 import useDataStore from '@/store/Store'
 import ColorBox from './ColorBox';
 import UploadButton from './UploadButton';
-
+import gsap from 'gsap';
 export default function FloatContainer() {
   const isAnimate = useDataStore((state) => state.isAnimate);
   const [visible, setVisible] = useState<boolean>(false);
   useEffect(() => {
     setVisible(isAnimate);
+    if(isAnimate){
+      gsap.to("#brand", { opacity: 0, duration: 1, ease: "power2.inOut", onComplete: () => {
+          document.getElementById("brand")?.classList.add("hidden");
+      }});
+    }
   }, [isAnimate])
   return (
     <div className={`${visible ? '' : 'hidden' } absolute inset-0 flex max-sm:flex-col gap-10 items-center justify-between p-10 z-30`}>
